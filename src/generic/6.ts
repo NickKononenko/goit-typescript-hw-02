02b52636375f9376f443257d0adbae5a3939131d
NickKononenko <kononenko0812@gmail.com>
2024-02-19 15:56:36 +0200

    done

--- src/generic/6.ts
+++ src/generic/6.ts
@@ -8,16 +8,15 @@
 */
 
 type User = {
-  name?: string;
-  surname?: string;
-  email?: string;
-  password?: string;
+  name: string;
+  surname: string;
+  email: string;
+  password: string;
 };
 
-function createOrUpdateUser({
-  email,
-  ...initialValues
-}: { email: string } & Partial<User>) {}
+function createOrUpdateUser(initialValues: Partial<User>) {
+  // Оновлення користувача
+}
 
 createOrUpdateUser({ email: "user@mail.com", password: "password123" });
 
